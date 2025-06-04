1f8f09cd19ab579c59d794ef720a7b352bc2f5f7
Jason Chen <windson7@gmail.com>
2025-06-03 21:46:51 -0700

    add event handler to tweet form

--- react-version/src/components/TweetForm.jsx
+++ react-version/src/components/TweetForm.jsx
@@ -1,7 +1,7 @@
 function TweetForm() {
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" onSubmit={(event) => event.preventDefault()}>
         <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
         <input type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
